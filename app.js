2b763709a44af34e10f430ec6f5a7206465e64e8
celestroccayoli <celestetroccayoli@gmail.com>
2022-01-06 11:04:21 -0300

    Port correction for unanimity between projects.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const publicPath = path.resolve( __dirname, "./public");
 
 app.use ( express.static(publicPath) );
  
-app.listen (2500, () => {console.log("Servidor corriendo en el puerto 2500")});
+app.listen (3000, () => {console.log("Servidor corriendo en el puerto 3000")});
 
 app.get("/", (req , res) => {
     res.sendFile( path.resolve (__dirname, "./views/home.html"))
